fd3d6b108cfb1ad258bf2caf38fb78ea8b220d75
--replace-all <anshulgupta4905@gmail.com>
2023-09-21 00:41:00 +0530

    Day-1

---
+++ solutions/1. Array/1. Search-an-Element-in-an-Array.cpp
@@ -0,0 +1,28 @@
+// ******** Given an integer array and another integer element. The task is to find if the given element is present in array or not ********
+
+// <======== Example ========>
+// Input: n = 4, arr[] = {1,2,3,4}, x = 3
+// Output: 2
+
+// Input: n = 5, arr[] = {1,2,3,4,5}, x = 5
+// Output: 4
+
+
+// Expected Time Complexity ==> O(n)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int search(int arr[], int N, int X) {
+        for(int i = 0; i < N; i++) {
+            if(arr[i] == X) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
