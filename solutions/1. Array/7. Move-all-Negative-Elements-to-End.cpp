6a72f482f81664fca0d570e4d6f75266c8021832
--replace-all <anshulgupta4905@gmail.com>
2023-09-22 00:55:40 +0530

    Day-2

---
+++ solutions/1. Array/7. Move-all-Negative-Elements-to-End.cpp
@@ -0,0 +1,41 @@
+// ******** Given an unsorted array arr[] of size N having both negative and positive integers. The task is place all negative element at the end of array without changing the order of positive element and negative element ********
+
+// <======== Example ========>
+// Input: N = 8, arr[] = {1, -1, 3, 2, -7, -5, 11, 6 }
+// Output: 1 3 2 11 6 -1 -7 -5
+
+// Input: N = 8, arr[] = {-5, 7, -3, -4, 9, 10, -1, 11}
+// Output: 7, 9, 10, 11, -5, -3, -4, -1
+
+
+// Expected Time Complexity ==> O(N)
+// Expected Auxiliary Space ==> O(N)
+
+
+
+
+class Solution{
+    public:
+    void segregateElements(int arr[],int n) {
+        int i = 0;
+        int temp[n];
+        
+        for(int j = 0; j < n; j++) {
+            if(arr[j] > 0) {
+                temp[i] = arr[j];
+                i++;
+            }
+        }
+        
+        for(int j = 0; j < n; j++) {
+            if(arr[j] < 0) {
+                temp[i] = arr[j];
+                i++;
+            }
+        }
+        
+        for(int j = 0; j < n; j++) {
+            arr[j] = temp[j];
+        }
+    }
+};
