d6c47a45ddc0e97cf1acc9e1919c5b01504cf8c0
--replace-all <anshulgupta4905@gmail.com>
2023-09-22 00:55:52 +0530

    Day-2

---
+++ solutions/1. Array/8. Missing-Number.cpp
@@ -0,0 +1,28 @@
+// ******** Vaibhav likes to play with numbers and he has N numbers. One day he was placing the numbers on the playing board just to count that how many numbers he has. He was placing the numbers in increasing order i.e. from 1 to N. But when he was putting the numbers back into his bag, some numbers fell down onto the floor. He picked up all the numbers but one number, he couldn't find. Now he has to go somewhere urgently, so he asks you to find the missing number ********
+// Note --> Don't use Sorting
+
+// <======== Example ========>
+// Input: N = 4, A[] = {1, 4, 3}
+// Output: 2
+
+// Input: N = 5, A[] = {2, 5, 3, 1}
+// Output: 4
+
+
+// Expected Time Complexity ==> O(N)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+int missingNumber(int A[], int N) {
+    int totalSum = N*(N+1) / 2;
+    int sum = 0;
+    
+    for(int i = 0; i < N-1; i++) {
+        sum += A[i];
+    }
+    
+    int ans = (totalSum - sum);
+    return ans;
+}
