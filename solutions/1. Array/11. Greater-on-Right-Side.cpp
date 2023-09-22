4b02a7b181daccb1d8296526a7cf152c261628a5
--replace-all <anshulgupta4905@gmail.com>
2023-09-23 00:47:02 +0530

    Day-3

---
+++ solutions/1. Array/11. Greater-on-Right-Side.cpp
@@ -0,0 +1,28 @@
+// ******** You are given an array Arr of size N. Replace every element with the next greatest element (greatest element on its right side) in the array. Also, since there is no element next to the last element, replace it with -1 ********
+
+// <======== Example ========>
+// Input: N = 6, Arr[] = {16, 17, 4, 3, 5, 2}
+// Output: 17 5 5 5 2 -1
+
+// Input: N = 4, Arr[] = {2, 3, 1, 9}
+// Output: 9 9 9 -1
+
+
+// Expected Time Complexity ==> O(N)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+public:	
+	void nextGreatest(int arr[], int n) {
+	    int replace = -1;
+	    
+	    for(int i = n-1; i >= 0; i--) {
+	        int temp = replace;
+	        replace = max(temp, arr[i]);
+	        arr[i] = temp;
+	    }
+	}
+};
