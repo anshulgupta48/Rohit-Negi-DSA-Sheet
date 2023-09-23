01f6b703888f81b521fcce75a47f9df0069c250a
--replace-all <anshulgupta4905@gmail.com>
2023-09-24 00:25:34 +0530

    Day-4

---
+++ solutions/1. Array/15. Equilibrium-Point.cpp
@@ -0,0 +1,38 @@
+// ******** Given an array A of n positive numbers. The task is to find the first equilibrium point in an array. Equilibrium point in an array is a position such that the sum of elements before it is equal to the sum of elements after it ********
+// Note  --> Return equilibrium point in 1-based indexing. Return -1 if no such point exists.
+
+// <======== Example ========>
+// Input: n = 5 , A[] = {1,3,5,2,2} 
+// Output: 3
+
+// Input: n = 1, A[] = {1}
+// Output: 1
+
+
+// Expected Time Complexity ==> O(N)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int equilibriumPoint(long long a[], int n) {
+        long long totalSum = 0;
+        long long sum = 0;
+        
+        for(int i = 0; i < n; i++) {
+            totalSum += a[i];
+        }
+        
+        for(int i = 0; i < n; i++) {
+            totalSum -= a[i];
+            if(totalSum == sum) {
+                return (i+1);
+            }
+            sum += a[i];
+        }
+        
+        return -1;
+    }
+};
