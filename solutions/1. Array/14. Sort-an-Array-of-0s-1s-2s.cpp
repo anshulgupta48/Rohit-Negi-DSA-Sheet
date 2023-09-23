b7682feca2ebc4843b5bb4495d4a8d402306e0c5
--replace-all <anshulgupta4905@gmail.com>
2023-09-24 00:25:22 +0530

    Day-4

---
+++ solutions/1. Array/14. Sort-an-Array-of-0s-1s-2s.cpp
@@ -0,0 +1,39 @@
+// ******** Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order ********
+
+// <======== Example ========>
+// Input: N = 5, arr[]= {0 2 1 2 0}
+// Output: 0 0 1 2 2
+
+// Input: N = 3, arr[] = {0 1 0}
+// Output: 0 0 1
+
+
+// Expected Time Complexity ==> O(N)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution {
+    public:
+    void sort012(int a[], int n) {
+        int start = 0;
+        int mid = 0;
+        int end = n - 1;
+        
+        while(mid <= end) {
+            if(a[mid] == 0) {
+                swap(a[start], a[mid]);
+                start++;
+                mid++;
+            }
+            else if(a[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(a[mid], a[end]);
+                end--;
+            }
+        }
+    }
+};
