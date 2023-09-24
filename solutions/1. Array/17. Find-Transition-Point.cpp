accd99ba833a5daef15eca4a831fbfae4b59ab6b
--replace-all <anshulgupta4905@gmail.com>
2023-09-25 01:01:59 +0530

    Day-5

---
+++ solutions/1. Array/17. Find-Transition-Point.cpp
@@ -0,0 +1,37 @@
+// ******** Given a sorted array containing only 0s and 1s, find the transition point ********
+
+// <======== Example ========>
+// Input: N = 5, arr[] = {0,0,0,1,1}
+// Output: 3
+
+// Input: N = 4, arr[] = {0,0,0,0}
+// Output: -1
+
+
+// Expected Time Complexity ==> O(logN)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+int transitionPoint(int arr[], int n) {
+    int start = 0;
+    int end = n-1;
+    
+    while(start <= end) {
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == 0) {
+            start = mid + 1;
+        }
+        else if(arr[mid] == 1) {
+            if(mid==0 || (mid>0 && arr[mid-1]==0)) {
+                return mid;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+    }
+    
+    return -1;
+}
