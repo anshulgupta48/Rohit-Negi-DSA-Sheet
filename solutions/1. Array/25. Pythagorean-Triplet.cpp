88ce11fae0d10f670fad9985b0a9a4b30acfab88
--replace-all <anshulgupta4905@gmail.com>
2023-09-27 01:26:30 +0530

    Day-7

---
+++ solutions/1. Array/25. Pythagorean-Triplet.cpp
@@ -0,0 +1,44 @@
+// ******** Given an array arr of N integers, write a function that returns true if there is a triplet (a, b, c) that satisfies a2 + b2 = c2, otherwise false ********
+
+// <======== Example ========>
+// Input: N = 5, Arr[] = {3, 2, 4, 6, 5}
+// Output: Yes
+
+// Input: N = 3, Arr[] = {3, 8, 5}
+// Output: No
+
+
+// Expected Time Complexity ==> O(max(Arr[i])2)
+// Expected Auxiliary Space ==> O(max(Arr[i]))
+
+
+
+
+class Solution{
+public:
+	bool checkTriplet(int arr[], int n) {
+	    for(int i = 0; i < n; i++) {
+	        arr[i] = arr[i] * arr[i];
+	    }
+	    
+	    sort(arr, arr+n);
+	    for(int i = n-1; i >= 2; i--) {
+	        int b = 0;
+	        int c = i-1;
+	        
+	        while(b < c) {
+	            if(arr[b] + arr[c] == arr[i]){
+                    return true;
+                }
+                else if (arr[b] + arr[c] < arr[i]) {
+                    b++;
+                }
+                else { 
+                    c--; 
+                }
+	        }
+	    }
+	    
+	    return false;
+	}
+};
