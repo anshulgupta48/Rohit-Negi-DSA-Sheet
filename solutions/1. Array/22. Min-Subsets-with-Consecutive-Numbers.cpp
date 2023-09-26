b535f42397ae4e54392043d378a39959626464c8
--replace-all <anshulgupta4905@gmail.com>
2023-09-26 13:25:58 +0530

    Day-6

---
+++ solutions/1. Array/22. Min-Subsets-with-Consecutive-Numbers.cpp
@@ -0,0 +1,31 @@
+// ******** Given an array of distinct positive numbers, the task is to calculate the minimum number of subsets (or subsequences) from the array such that each subset contains consecutive numbers ********
+
+// <======== Example ========>
+// Input: N = 10, Arr[] = {100, 56, 5, 6, 102, 58, 101, 57, 7, 103} 
+// Output: 3
+
+// Input: N = 3, Arr[] = {10, 100, 105}
+// Output: 3
+
+
+// Expected Time Complexity ==> O(N*log(N))
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int numofsubset(int arr[], int n) {
+        int count = 1;
+        sort(arr, arr+n);
+        
+        for(int i = 0; i < n-1; i++) {
+            if(arr[i+1]-arr[i] != 1) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
