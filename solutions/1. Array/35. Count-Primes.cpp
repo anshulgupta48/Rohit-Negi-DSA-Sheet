d5544780859e1fce3ab13692d8383ad4d9d40d6c
--replace-all <anshulgupta4905@gmail.com>
2023-10-08 01:26:01 +0530

    Day-10

---
+++ solutions/1. Array/35. Count-Primes.cpp
@@ -0,0 +1,35 @@
+// ******** Given an integer n, return the number of prime numbers that are strictly less than n ********
+
+// <======== Example ========>
+// Input: n = 10
+// Output: 4
+
+// Input: n = 0
+// Output: 0
+
+// Input: n = 1
+// Output: 0
+
+
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count = 0;
+        vector<bool> prime(n+1, true);
+        prime[0] = prime[1] = false;
+
+        for(int i = 2; i < n; i++) {
+            if(prime[i]) {
+                count++;
+
+                for(int j = 2*i; j < n; j+=i) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        return count;
+    }
+};
