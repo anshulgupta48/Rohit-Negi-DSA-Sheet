b0c97746c832890423fa6e812b395b63ea68d1c8
--replace-all <anshulgupta4905@gmail.com>
2023-10-09 01:07:30 +0530

    Day-11

---
+++ solutions/1. Array/38. Majority-Element.cpp
@@ -0,0 +1,3 @@
+// ******** Find the majority element in the array. A majority element in an array A[] of size n is an element that appears more than n/2 times (and hence there is at most one such element) ********
+
+// Just Go & Read the Article and Check Solutions --> https://www.geeksforgeeks.org/majority-element/
