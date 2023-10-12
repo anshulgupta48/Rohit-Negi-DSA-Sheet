d2fbc79981e8d7089b9d3d494f0c5c02814fe97f
--replace-all <anshulgupta4905@gmail.com>
2023-10-12 12:08:07 +0530

    Day-15

---
+++ solutions/2. String/47. Check-if-String-are-Rotation-of-Each-Other-or-Not.cpp
@@ -0,0 +1,31 @@
+// ******** Given two strings s1 and s2. The task is to check if s2 is a rotated version of the string s1. The characters in the strings are in lowercase ********
+
+// <======== Example ========>
+// Input: geeksforgeeks, forgeeksgeeks
+// Output: 1
+
+// Input: mightandmagic, andmagicmigth
+// Output: 0
+
+
+// Expected Time Complexity ==> O(N).
+// Expected Auxiliary Space ==> O(N).
+
+
+
+
+class Solution {
+    public:
+    bool areRotations(string s1,string s2) {
+        if(s1.size() != s2.size()) {
+            return false;
+        }
+        
+        s1 = s1 + s1;
+        if(s1.find(s2) != -1) {
+            return true;
+        }
+        
+        return false;
+    }
+};
