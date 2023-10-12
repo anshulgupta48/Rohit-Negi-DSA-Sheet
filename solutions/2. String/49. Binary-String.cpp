b3d8a9d5c49612632fdabfbc385f3753647e5215
--replace-all <anshulgupta4905@gmail.com>
2023-10-12 12:08:32 +0530

    Day-15

---
+++ solutions/2. String/49. Binary-String.cpp
@@ -0,0 +1,29 @@
+// ******** Given a binary string S. The task is to count the number of substrings that start and end with 1. For example, if the input string is “00100101”, then there are three substrings “1001”, “100101” and “101” ********
+
+// <======== Example ========>
+// Input: N = 4, S = 1111
+// Output: 6
+
+// Input: N = 5, S = 01101
+// Output: 3
+
+
+// Expected Time Complexity ==> O(N).
+// Expected Auxiliary Space ==> O(1).
+
+
+
+
+class Solution {
+    public:
+    long binarySubstring(int n, string a){
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(a[i] == '1') {
+                count++;
+            }
+        }
+        
+        return (count*(count-1))/2;
+    }
+};
