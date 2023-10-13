d7bd7ee444aa9dc91d778107410c65f8740b7262
--replace-all <anshulgupta4905@gmail.com>
2023-10-13 12:08:44 +0530

    Day-16

---
+++ solutions/2. String/51. Merge-Two-Strings.cpp
@@ -0,0 +1,37 @@
+// ******** Given two strings S1 and S2 as input, the task is to merge them alternatively i.e. the first character of S1 then the first character of S2 and so on till the strings end ********
+// NOTE --> Add the whole string if other string is empty.
+
+// <======== Example ========>
+// Input: S1 = "Hello", S2 = "Bye"
+// Output: HBeylelo
+
+// Input: S1 = "abc", S2 = "def"
+// Output: adbecf
+
+
+// Expected Time Complexity ==> O(|S1| + |S2|).
+// Expected Auxiliary Space ==> O(1).
+
+
+
+
+string merge (string S1, string S2) {
+    string ans = "";
+    int n = S1.length(), m = S2.length();
+    int i = 0, j = 0;
+    
+    while(i < n && j < m) {
+        ans += S1[i++];
+        ans += S2[j++];
+    }
+    
+    while(i != n) {
+        ans += S1[i++];
+    }
+    
+    while(j != m) {
+        ans += S2[j++];
+    }
+    
+    return ans;
+}
