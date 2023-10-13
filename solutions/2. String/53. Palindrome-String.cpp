36bdadd849ebcdefc381c6ceb1e0d7c7bd72161a
--replace-all <anshulgupta4905@gmail.com>
2023-10-13 12:09:12 +0530

    Day-16

---
+++ solutions/2. String/53. Palindrome-String.cpp
@@ -0,0 +1,34 @@
+// ******** Given a string S, check if it is palindrome or not ********
+
+// <======== Example ========>
+// Input: S = "abba"
+// Output: 1
+
+// Input: S = "abc"
+// Output: 0
+
+
+// Expected Time Complexity ==> O(Length of S)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+public:
+	int isPalindrome(string S) {
+	    int start = 0;
+	    int end = S.length() - 1;
+	    
+	    while(start <= end) {
+	        if(S[start] != S[end]) {
+	            return 0;
+	        }
+	        
+	        start++;
+	        end--;
+	    }
+	    
+	    return 1;
+	}
+};
