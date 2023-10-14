82e57472632f7ebdb527d32bb5fea2c9ca863e76
--replace-all <anshulgupta4905@gmail.com>
2023-10-14 11:49:13 +0530

    Day-17

---
+++ solutions/2. String/56. Length-of-Last-Word.cpp
@@ -0,0 +1,37 @@
+// ******** Given a string s consisting of words and spaces, return the length of the last word in the string. A word is a maximal substring consisting of non-space characters only ********
+
+// <======== Example ========>
+// Input: s = "Hello World"
+// Output: 5
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+
+
+
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        bool flag = 0;
+        int ans = 0;
+
+        for(int i = n-1; i >= 0; i--) {
+            if(s[i] != ' ') {
+                ans++;
+                flag = 1;
+            }
+            else {
+                if(flag == 1) {
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
