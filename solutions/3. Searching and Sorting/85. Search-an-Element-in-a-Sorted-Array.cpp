6da8a3d217a570c82ecb7a718646a579d30d91cf
--replace-all <anshulgupta4905@gmail.com>
2023-12-05 12:59:58 +0530

    Day-29

---
+++ solutions/3. Searching and Sorting/85. Search-an-Element-in-a-Sorted-Array.cpp
@@ -0,0 +1,38 @@
+// ******** Given an array arr[] sorted in ascending order of size N and an integer K. Check if K is present in the array or not. ********
+
+// <======== Example ========>
+// Input: N = 5, K = 6, arr[] = {1,2,3,4,6}
+// Output: 1
+
+// Input: N = 5, K = 2, arr[] = {1,3,4,5,6}
+// Output: -1
+
+
+// Expected Time Complexity ==> O(Log N)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution{
+    public:
+    int searchInSorted(int arr[], int N, int K) {
+        int start = 0;
+        int end = N-1;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == K) {
+                return 1;
+            }
+            else if(arr[mid] > K) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+};
