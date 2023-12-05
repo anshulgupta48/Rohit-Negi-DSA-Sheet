28e367ee9a8c688ead38e14961f7ef3cd60d49a9
--replace-all <anshulgupta4905@gmail.com>
2023-12-05 13:01:15 +0530

    Day-30

---
+++ solutions/3. Searching and Sorting/91. First-and-Last-Occurrence-of-X.cpp
@@ -0,0 +1,64 @@
+// ******** Given a sorted array having N elements, find the indices of the first and last occurrences of an element X in the given array. If the number X is not found in the array, return '-1' as an array. ********
+
+// <======== Example ========>
+// Input: N = 4 , X = 3, arr[] = { 1, 3, 3, 4 }
+// Output: 1 2
+
+// Input: N = 4, X = 5, arr[] = { 1, 2, 3, 4 }
+// Output: -1
+
+
+// Expected Time Complexity ==> O(logN)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution {
+  public:
+    vector<int> firstAndLast(vector<int> &arr, int n, int x) {
+        vector<int> ans;
+        int a = -1, b = -1;
+        int start = 0;
+        int end = n-1;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == x) {
+                a = mid;
+                end = mid - 1;
+            }
+            else if(arr[mid] > x) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        
+        start = 0, end = n - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == x) {
+                b = mid;
+                start = mid + 1;
+            }
+            else if(arr[mid] < x) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        
+        if(a<0 && b<0) {
+            ans.push_back(-1);
+        }
+        else {
+            ans.push_back(a);
+            ans.push_back(b);
+        }
+        
+        return ans;
+    }
+};
