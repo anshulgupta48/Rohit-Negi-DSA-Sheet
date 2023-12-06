3ade2d53092e81ba94dcd3370a3b89a5c60d9b3b
--replace-all <anshulgupta4905@gmail.com>
2023-12-06 12:37:00 +0530

    Day-31

---
+++ solutions/3. Searching and Sorting/94. Selection-Sort.cpp
@@ -0,0 +1,31 @@
+// ******** Given an unsorted array of size N, use selection sort to sort arr[] in increasing order. ********
+
+// <======== Example ========>
+// Input: N = 5, arr[] = {4, 1, 3, 9, 7}
+// Output: 1 3 4 7 9
+
+// Input: N = 10, arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+// Output: 1 2 3 4 5 6 7 8 9 10
+
+
+// Expected Time Complexity ==> O(N^2)
+// Expected Auxiliary Space ==> O(1)
+
+
+
+
+class Solution {
+    public:
+    void selectionSort(int arr[], int n) {
+       for(int i = 0; i < n; i++) {
+           int min_index = i;
+           for(int j = i+1; j < n; j++) {
+               if(arr[j] < arr[min_index]) {
+                   min_index = j;
+               }
+           }
+           
+           swap(arr[i], arr[min_index]);
+       }
+    }
+};
