e1da4bae05b6d85a2e49091f2bd50ea6c83be4df
--replace-all <anshulgupta4905@gmail.com>
2023-12-10 17:34:18 +0530

    Day-35

--- solutions/3. Searching and Sorting/112. Smallest-Positive-Missing-Number.cpp
+++ solutions/3. Searching and Sorting/112. Smallest-Positive-Missing-Number.cpp
@@ -1,5 +1,5 @@
 // ******** You are given an array arr[] of N integers. The task is to find the smallest positive number missing from the array. ********
-// Note -->  Positive number starts from 1.
+// Note --> Positive number starts from 1.
 
 // <======== Example ========>
 // Input: N = 5, arr[] = {1,2,3,4,5}
