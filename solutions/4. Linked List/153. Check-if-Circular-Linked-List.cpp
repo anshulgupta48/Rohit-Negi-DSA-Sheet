d7cf4dfaed457bf2a8f6e12c384e5ccd68616564
--replace-all <anshulgupta4905@gmail.com>
2023-12-24 13:25:35 +0530

    Day-50

---
+++ solutions/4. Linked List/153. Check-if-Circular-Linked-List.cpp
@@ -0,0 +1,28 @@
+// ******** Given head, the head of a singly linked list, find if the linked list is circular or not. A linked list is called circular if it not NULL terminated and all nodes are connected in the form of a cycle. An empty linked list is considered as circular. ********
+// Note --> The linked list does not contains any inner loop.
+
+// <======== Example ========>
+// Input: LinkedList: 1->2->3->4->5
+// Output: 1
+
+// Input: LinkedList: 2->4->6->7->5->1
+// Output: 0
+
+
+// Expected Time Complexity ==> O(N).
+// Expected Auxiliary Space ==> O(1).
+
+
+
+
+bool isCircular(Node *head) {
+    Node *curr = head;
+    while(curr->next != NULL) {
+        curr = curr->next;
+        if(curr == head) {
+            return true;
+        }
+    }
+    
+    return false;
+}
