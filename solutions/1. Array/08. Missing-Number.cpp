0e79ed9de2595d6e870f841d717543e1a6bd9b0b
anshulgupta48 <anshulgupta4905@gmail.com>
2024-02-16 09:35:26 +0530

    Day-2

--- solutions/1. Array/08. Missing-Number.cpp
+++ solutions/1. Array/08. Missing-Number.cpp
@@ -16,12 +16,12 @@
 
 
 int missingNumber(int A[], int N) {
-    int totalSum = N*(N+1) / 2;
+    int totalSum = n * (n+1) / 2;
     int sum = 0;
-    
-    for(int i = 0; i < N-1; i++) {
-        sum += A[i];
+        
+    for(int i = 0; i < n-1; i++) {
+        sum += arr[i];
     }
-    
+        
     return (totalSum - sum);
 }
