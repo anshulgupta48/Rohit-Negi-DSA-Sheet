fba967ec0cc42e945303a7d3c59aebca6c1717b3
anshulgupta48 <anshulgupta4905@gmail.com>
2024-03-14 08:54:24 +0530

    Day-3

--- solutions/1. Array/09. Search-a-2D-Matrix.cpp
+++ solutions/1. Array/09. Search-a-2D-Matrix.cpp
@@ -18,13 +18,12 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row = matrix.size();
         int col = matrix[0].size();
-
         int start = 0;
         int end = row*col - 1;
+
         while(start <= end) {
             int mid = start + (end - start) / 2;
             int element = matrix[mid/col][mid%col];
-            
             if(element == target) {
                 return true;
             }
