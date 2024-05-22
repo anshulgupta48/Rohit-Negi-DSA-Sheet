0b81f8d447ddd959a30fc1c7df0e90daa9a4f6b3
anshulgupta48 <anshulgupta4905@gmail.com>
2024-05-22 13:08:05 +0530

    Day-6

--- solutions/1. Array/23. Find-Missing-and-Repeating.cpp
+++ solutions/1. Array/23. Find-Missing-and-Repeating.cpp
@@ -18,7 +18,6 @@ class Solution{
 public:
     vector<int> findTwoElement(vector<int> arr, int n) {
         vector<int> ans(2);
-        
         for(int i = 0; i < n; i++) {
             if(arr[abs(arr[i])-1] > 0) {
                 arr[abs(arr[i])-1] = -arr[abs(arr[i])-1];
