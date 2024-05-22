38bf9b6be0bd087f2be5922a9bc4701bbad17a5c
anshulgupta48 <anshulgupta4905@gmail.com>
2024-05-22 13:58:42 +0530

    Day-9

--- solutions/1. Array/33. Next-Permutaion.cpp
+++ solutions/1. Array/33. Next-Permutaion.cpp
@@ -18,16 +18,15 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int i = n-2;
+        int j = n-1;
 
         while(i >= 0 && nums[i] >= nums[i+1]) {
             i--;
         }
         if(i < 0) {
-            reverse(nums.begin(), nums.end());
-            return;
+            return reverse(nums.begin(), nums.end());
         }
 
-        int j = n-1;
         while(j > i && nums[j] <= nums[i]) {
             j--;
         }
