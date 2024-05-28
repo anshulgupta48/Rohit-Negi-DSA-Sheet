127e10b47617c448316a37200ea0312c1cef945b
anshulgupta48 <anshulgupta4905@gmail.com>
2024-05-28 14:27:22 +0530

    Day-3

--- solutions/1. Array/10. Find-the-Fine.cpp
+++ solutions/1. Array/10. Find-the-Fine.cpp
@@ -17,23 +17,23 @@
 class Solution{
   public:
     long long int totalFine(int n, int date, int car[], int fine[]) {
-        long long int totalFine = 0;
+        long long int ans = 0;
         
         if(date%2 == 0) {
             for(int i = 0; i < n; i++) {
                 if(car[i]%2 != 0) {
-                    totalFine += fine[i];
+                    ans += fine[i];
                 }
             }
         }
         else {
             for(int i = 0; i < n; i++) {
                 if(car[i]%2 == 0) {
-                    totalFine += fine[i];
+                    ans += fine[i];
                 }
             }
         }
         
-        return totalFine;
+        return ans;
     }
 };
