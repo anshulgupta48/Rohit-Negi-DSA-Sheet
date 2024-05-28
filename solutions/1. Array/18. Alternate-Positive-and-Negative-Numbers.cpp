d4c442d7738d10eaf90097e0a845fc25c96fd01a
anshulgupta48 <anshulgupta4905@gmail.com>
2024-05-28 14:37:11 +0530

    Day-5

--- solutions/1. Array/18. Alternate-Positive-and-Negative-Numbers.cpp
+++ solutions/1. Array/18. Alternate-Positive-and-Negative-Numbers.cpp
@@ -25,15 +25,15 @@ public:
 	        if(arr[i] >= 0) {
 	            pos.push_back(arr[i]);
 	        }
-	        else {
+	    }
+	    
+	    for(int i = 0; i < n; i++) {
+	        if(arr[i] < 0) {
 	            neg.push_back(arr[i]);
 	        }
 	    }
 	    
-	    int i = 0;
-	    int j = 0;
-	    int k = 0;
-	    
+	    int i = 0, j = 0, k = 0;
 	    while(i < pos.size() && j < neg.size()) {
 	        arr[k++] = pos[i++];
 	        arr[k++] = neg[j++];
