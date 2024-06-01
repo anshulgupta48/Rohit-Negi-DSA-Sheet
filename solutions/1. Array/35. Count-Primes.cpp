e527b821fbb670dd34b495dc331507c2b6ab7fef
anshulgupta48 <anshulgupta4905@gmail.com>
2024-06-01 14:44:03 +0530

    Day-10

--- solutions/1. Array/35. Count-Primes.cpp
+++ solutions/1. Array/35. Count-Primes.cpp
@@ -22,6 +22,7 @@ public:
         for(int i = 2; i < n; i++) {
             if(isPrime[i] == true) {
                 count++;
+
                 for(int j = 2*i; j <= n; j += i) {
                     isPrime[j] = false;
                 }
