72b3d9128cd283767ff62763c59ea93df7d7309d
anshulgupta48 <anshulgupta4905@gmail.com>
2024-07-02 07:25:55 +0530

    Day-1

--- solutions/1. Array/04. Cyclically-Rotate-an-Array-by-One.cpp
+++ solutions/1. Array/04. Cyclically-Rotate-an-Array-by-One.cpp
@@ -14,8 +14,10 @@
 
 
 
-void rotate(int arr[], int n) {
+void rotate(int arr[]) {
+    int n = arr.size();
     int temp = arr[n-1];
+        
     for(int i = n-1; i > 0; i--) {
         arr[i] = arr[i-1];
     }
