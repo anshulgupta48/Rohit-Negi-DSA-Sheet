2fc30acacf672d61766ce06fe579d8d52d8cd65d
anshulgupta48 <anshulgupta4905@gmail.com>
2024-07-07 09:02:31 +0530

    Day-18

--- solutions/2. String/60. Meta-String.cpp
+++ solutions/2. String/60. Meta-String.cpp
@@ -19,18 +19,21 @@ bool metaStrings (string S1, string S2) {
         return false;
     }
     
+    int n = S1.length();
     int i = 0;
-    int j = S1.length()-1;
+    int j = n-1;
+    
     while(S1[i] == S2[i]) {
         i++;
     }
     while(S1[j] == S2[j]) {
         j--;
     }
-    swap(S1[i], S1[j]);
     
+    swap(S1[i], S1[j]);
     if(S1 == S2) {
         return true;
     }
+    
     return false;
 }
