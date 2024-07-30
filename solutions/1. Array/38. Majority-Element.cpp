ae604b1f0823a828d4598ec8b2748cd95bced4e9
anshulgupta48 <anshulgupta4905@gmail.com>
2024-07-30 22:10:29 +0530

    Day-11

--- solutions/1. Array/38. Majority-Element.cpp
+++ solutions/1. Array/38. Majority-Element.cpp
@@ -15,7 +15,7 @@
 
 int majorityElement(int a[], int size) {
     int element = -1;
-    int count = 0;
+    int count = 0; 
         
     for(int i = 0; i < size; i++) {
         if(count == 0 || a[i] == element) {
