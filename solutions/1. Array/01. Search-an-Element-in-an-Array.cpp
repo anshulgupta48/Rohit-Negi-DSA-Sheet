301229749bbde78b745c75c5b4923196b84a0d57
anshulgupta48 <anshulgupta4905@gmail.com>
2024-08-07 08:00:07 +0530

    Day-1

--- solutions/1. Array/01. Search-an-Element-in-an-Array.cpp
+++ solutions/1. Array/01. Search-an-Element-in-an-Array.cpp
@@ -18,6 +18,7 @@ class Solution{
     public:
     int search(vector<int>& arr, int x) {
         int n = arr.size();
+        
         for(int i = 0; i < n; i++) {
             if(arr[i] == x) {
                 return i;
--- solutions/1. Array/04. Cyclically-Rotate-an-Array-by-One.cpp
+++ solutions/1. Array/04. Cyclically-Rotate-an-Array-by-One.cpp
@@ -16,7 +16,7 @@
 
 void rotate(vector<int> &arr) {
     int n = arr.size();
-    int temp = arr[n-1]; 
+    int temp = arr[n-1];
         
     for(int i = n-1; i > 0; i--) {
         arr[i] = arr[i-1];
