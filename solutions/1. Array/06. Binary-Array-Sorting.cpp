3f93aa9642a670613a2a996f8d9175ad90696572
anshulgupta48 <anshulgupta4905@gmail.com>
2024-08-07 08:01:47 +0530

    Day-2

--- solutions/1. Array/06. Binary-Array-Sorting.cpp
+++ solutions/1. Array/06. Binary-Array-Sorting.cpp
@@ -18,6 +18,7 @@ class Solution{
   public:
     void binSort(int A[], int N) {
         int zeroCount = 0;
+        
         for(int i = 0; i < N; i++) {
             if(A[i] == 0) {
                 zeroCount++;
